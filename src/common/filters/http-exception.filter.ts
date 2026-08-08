1f9c61fc732d233ffc26b5afa4f83e469fc3bfc1
elfplayer937187 <elfplayer937187@gmail.com>
2026-08-08 19:03:46 +0800

    feat: 处理404异常

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -4,13 +4,22 @@ import {
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 @Catch()
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response: Response = ctx.getResponse();
-
+    const request: Request = ctx.getRequest<Request>();
+    const response: Response = ctx.getResponse<Response>();
+    //属于404错误
+    if (exception instanceof HttpException && exception.getStatus() === 404) {
+      response.status(404).json({
+        code: 404,
+        message: `接口${request.method}-${request.url}不存在`,
+        data: null,
+        ok: false,
+      });
+    }
     // 属于业务型错误
     if (exception instanceof HttpException) {
       const status = exception.getStatus();
@@ -21,6 +30,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
         return;
       }
     }
+
     // 未知错误 → 服务繁忙
     console.error('Unhandled error:', exception);
     response.status(200).json({
